653b3c13798b33ca3ed367a460133d295387a47d
jiahuiwang2023 <wangjiahui201709@outlook.com>
2025-01-31 19:20:24 -0600

    add No.1.py

--- DSA_Python/No.1.py
+++ DSA_Python/No.1.py
@@ -5,7 +5,7 @@
 # Array
 # Hash Table
 
-# solution:
+# Brute force solution:
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
@@ -16,7 +16,7 @@ class Solution:
 
 # Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
-# Better solution:
+# Hash map (one pass) solution:
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
