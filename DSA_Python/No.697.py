4e9f3456ec11b0e23a2cd2a2bb058f196d0f13ee
jiahuiwang2023 <wangjiahui201709@outlook.com>
2025-02-03 10:08:25 -0600

    "add No.697.py"

---
+++ DSA_Python/No.697.py
@@ -0,0 +1,23 @@
+# 697. Degree of an Array
+
+# Easy
+
+# Array
+# Hash Table
+
+# hash table solution:
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        left, right = dict(), dict()
+        counter = collections.Counter()
+        for i, num in enumerate(nums):
+            if num not in left:
+                left[num] = i
+            right[num] = i
+            counter[num] += 1
+        degree = max(counter.values())
+        res = len(nums)
+        for k, v in counter.items():
+            if v == degree:
+                res = min(res, right[k] - left[k] + 1)
+        return res
